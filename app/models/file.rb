2f05af471b0188d5c84a305bc7fa96195f1190b2
poyasharonlin <sharon.lin@iss.nthu.edu.tw>
2025-04-02 15:48:01 +0800

    add: add app/models/file.rb

---
+++ app/models/file.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'base64'
+require 'rbnacl'
+
+module UCCMe
+    STORE_DIR = 'db/local'
+
+    # store properties 
+    class File 
+        def initialize(new_file)
+            @id = new_file['id'] || new_id
+            @filename = new_file['filename']
+            @description = new_file['description']
+            @content = new_file['content']
+            @cc_types = new_file['cc_types']
+        end 
+
+        attr_reader :id, :filename, :description, :content, :cc_types
+
+        def to_json(options = {})
+            JSON(
+                {
+                    type: 'file',
+                    id:,
+                    filename:,
+                    description:,
+                    content:,
+                    cc_types:
+                },
+                options
+            )
+        end 
+
+        # set up location to store file
+        def self.locate
+            FileUtils.mkdir_p(UCCMe::STORE_DIR)
+        end
+
+        # create
+        def save 
+            File.write("#{UCCMe::STORE_DIR}/#{id}.txt", to_json)
+        end 
+
+        # read 1 file 
+        def self.find(file_id)
+            temp_json = File.read("#{UCCMe::STORE_DIR}/#{file_id}.txt")
+            File.new JSON.parse(temp_json)
+        end 
+
+
+        def self.all
+            Dir.glob("#{UCCMe::STORE_DIR}/*.txt").map do |file|
+                file.match(%r{#{Regexp.quote(UCCMe::STORE_DIR)}/(.*)\.txt})[1] # retrieve index 
+                # file.match(%r{#{Regexp.quote(UCCMe::STORE_DIR)}/(.*)\.txt})[2] # retrieve filename 
+            end 
+        end 
+
+        private
+
+        def new_id 
+            timestamp = Time.now.to_f.to_json
+            Base64.urlsafe_encode64(RbNaCl::Hash.sha256(timestamp))[0..9]
+        end 
+    end 
+end
