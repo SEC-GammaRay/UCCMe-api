36ecb3bce9f741f71315e2b025fa58534f9920b0
poyasharonlin <sharon.lin@iss.nthu.edu.tw>
2025-04-17 00:29:02 +0800

    add: db files

---
+++ db/migrations/002_files_create.rb
@@ -0,0 +1,20 @@
+require 'sequel'
+
+Sequel.migration do
+  change do
+    create_table(:files) do
+      primary_key :id
+      foreign_key :folder_id, table: :folders
+      String :filename, unique: true, null: false
+      String :description, null: false
+      String :content, null: false
+      String :cc_types, null: false
+      DateTime :created_at, default: Sequel::CURRENT_TIMESTAMP
+      DateTime :updated_at, default: Sequel::CURRENT_TIMESTAMP, on_update: Sequel::CURRENT_TIMESTAMP
+      unique [ :folder_id, :filename ]
+    end
+
+    # add index to filename 
+    add_index(:files, :filename)
+  end
+end
