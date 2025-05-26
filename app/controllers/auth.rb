2b710311ed1e0841f36abd95ae3766d23c938d26
poyasharonlin <sharon.lin@iss.nthu.edu.tw>
2025-05-26 10:16:40 +0800

    add: add register routing

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -7,6 +7,26 @@ module UCCMe
   # Web controller for UCCMe API
   class Api < Roda
     route('auth') do |routing|
+      routing.on 'register' do 
+        # POST /api/v1/auth/register
+        routing.post do
+          reg_data = JSON.parse(routing.body.read, symbolize_names: true)
+          VerifyRegistration.new(reg_data).call
+
+          response.status = 202 
+          { message: 'Verification email sent'}.to_json
+        rescue VerifyRegistration::InvalidRegistration => e
+          routing.hait 400, { message: e.message }.to_json
+        rescue VerifyRegistration::EmailProviderError => e
+          Api.logger.error "Could not send email to: #{e.inspect}"
+          routing.halt 500, { message: 'Error sending email'}.to_json
+        rescue StandardError => e 
+          Api.logger.error "Could not verify registeration: #{e.inspect}"
+          routing.halt 500
+        end 
+      end 
+
+      
       routing.is 'authenticate' do
         # POST /api/v1/auth/authenticate
         routing.post do
